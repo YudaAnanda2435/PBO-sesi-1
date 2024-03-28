53fff7c2f001a323f1453540c1bb91546e051676
YudaAnanda245 <0803yudha@gmail.com>
2024-03-28 14:13:42 +0700

    jgjgjg

--- Tugas4/lingkaran.java
+++ Tugas4/lingkaran.java
@@ -1,10 +1,12 @@
 package Tugas4;
 
 public class Lingkaran extends BangunDatar {
-    public Lingkaran(double radius){
+    public Lingkaran(double radius) {
         this.hitungLuas(radius);
         this.hitungKeliling(radius);
     }
+
+    
     public void hitungLuas(double radius) {
         super.luas = 3.14 * radius * radius;
     }
